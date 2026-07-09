608e9252cb4d12c2cfe39e555d688d30eb0ae72a
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-07-09 18:12:13 -0500

    Added up/down movement for Coin.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
 
-    [SerializeField] private int rotationSpeed = 10;
+    private float rotationSpeed = 10f;
+    [SerializeField] private float moveSpeed = 10f;
+    private bool isOnGround;
+    private float yBound;
+    private Rigidbody coinRb;
+    [SerializeField] private float bounce = .5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        coinRb = GetComponent<Rigidbody>();
         Physics.IgnoreLayerCollision(6, 7);
     }
 
@@ -15,11 +21,27 @@ public class Coin : MonoBehaviour
     void Update()
     {
         Rotate();
+        MoveUpAndDown();
     }
-
+    /// <summary>
+    /// Makes the coin rotate on y axis continuously
+    /// </summary>
     void Rotate()
     {
         transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    void MoveUpAndDown()
+    {
+        if (isOnGround == true)
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -27,6 +49,11 @@ public class Coin : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            coinRb.AddForce(Vector3.up * bounce, ForceMode.Impulse);
         }
     }
 }
