91af8cbfc30e74576922fdb3d3f5cad7f43ec12e
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-07-10 12:47:09 -0500

    Added Obstacle Prefabs and Started working on spawner

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject[] obstaclePrefabs;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private float spawnInterval;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacles", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacles() 
+    {
+        Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], new Vector3(26, 1.5f, -29), Quaternion.identity);
     }
 }
