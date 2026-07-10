b6c66d6c1343b4afa09ecc35e09f964a542ac0f4
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-07-10 11:39:00 -0500

    CoinSpawner.cs implemented

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
+    [SerializeField] private Rigidbody[] coinPrefabs;
+    public float CoinInterval;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        for (int i = 0; i < 5; i++)
+        {
+            Invoke("SpawnCoinsAtStart", .2f);
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnCoinsOnTimer();
+    }
+
+    void SpawnCoinsAtStart()
+    {
+        float xPosition = Random.Range(-15f, 15f);
+        float yPosition = 1;
+        float zPosition = Random.Range(-15f, 15f);
+
+        Vector3 spawnPos = new Vector3(xPosition, yPosition, zPosition);
+        Instantiate(coinPrefabs[Random.Range(0, coinPrefabs.Length)], spawnPos, Quaternion.identity);
+    }
+
+    void SpawnCoinsOnTimer()
+    {
+
     }
 }
