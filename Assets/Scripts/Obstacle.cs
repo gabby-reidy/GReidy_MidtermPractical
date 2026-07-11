205c7e3e4884fa454812f59208f301f4c4705218
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-07-11 15:13:55 -0500

    ObstacleSpawner.cs working properly
    Fixed issue where obstacles would only spawn a set amount and then never spawn again. Created DestroyAndRespawn method to handle this.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,14 +5,18 @@ public class Obstacle : MonoBehaviour
     private int damage;
     private float speed;
     private Vector3 direction;
+    private Rigidbody obstacleRb;
+    private ObstacleSpawner obstacleSpawner;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Physics.IgnoreLayerCollision(6, 7);
+        obstacleRb = GetComponent<Rigidbody>();
         damage = Random.Range(1, 6);
         speed = Random.Range(5, 15);
-        direction = new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f)).normalized;
+        direction = new Vector3(Random.Range(-5f, 5f), 1f, Random.Range(-5f, 5f)).normalized;
+        obstacleRb.linearVelocity = direction * speed;
     }
 
     // Update is called once per frame
@@ -20,16 +24,25 @@ public class Obstacle : MonoBehaviour
     {
         transform.Translate(direction * speed * Time.deltaTime);
     }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            DestroyAndRespawn();
         }
         else if (collision.gameObject.CompareTag("Wall"))
         {
-            Destroy(gameObject);
+            DestroyAndRespawn();
+        }
+    }
+
+    private void DestroyAndRespawn()
+    {
+        Destroy(gameObject);
+        if (obstacleSpawner != null)
+        {
+            obstacleSpawner.SpawnObstacles();
         }
     }
 }
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -4,34 +4,32 @@ public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] obstaclePrefabs;
     [SerializeField] private Transform[] spawnPoints;
-    [SerializeField] private float spawnDelay = 2f;
-    [SerializeField] private float spawnInterval = 5f;
-    private float obstacleCount = 0;
+    [SerializeField] private float spawnDelay = 1f;
+    [SerializeField] private float spawnInterval = 2f;
+    public int ObstacleCount;
     private float maxObstacleCount = 6;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        InvokeRepeating("SpawnObstacles", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (obstacleCount <= maxObstacleCount)
-        {
-            SpawnObstacles();
-        }
+            
     }
 
-    void SpawnObstacles() 
+    public void SpawnObstacles() 
     {
         int spawnIndex = Random.Range(0, spawnPoints.Length);
         Transform randomSpawnPoint = spawnPoints[spawnIndex];
 
-        Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], randomSpawnPoint.position, Quaternion.identity);
-
-        obstacleCount++;
+        GameObject spawn = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], randomSpawnPoint.position, Quaternion.identity);
+        Obstacle obstacleRef = spawn.GetComponent<Obstacle>();
+  
+        ObstacleCount++;
     }
 }
